2df46b3ec26bd1b427a5b1309832bf43b6481c2a
Sarah Schütz <sarahschuetz@gmx.at>
2015-11-07 16:57:27 +0200

    Menu added

---
+++ Assets/Scripts/Menu.js
@@ -0,0 +1,32 @@
+﻿var quit = false;
+
+function OnMouseEnter() {
+
+    GetComponent.<Renderer>().material.color = Color.red;		//Change Color to red!
+
+}
+
+function OnMouseExit() {
+
+    GetComponent.<Renderer>().material.color = Color.white;		//Change Color to white!
+
+}
+
+function OnMouseUp() {
+
+    if (quit == true) {
+        Application.Quit();					    //If you click on quit aplication quits.
+    }
+    else {
+        Application.LoadLevel(1);				//If you click on other button it loads game!
+    }
+
+}
+
+function Update() {
+    if (Input.GetKey(KeyCode.Escape)) 				//If you press (escape) game force closes!
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Scripts/handlContiousnes.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class handlContiousnes : MonoBehaviour {
+
+	private CanvasGroup black;
+
+	// Use this for initialization
+	void Start () {
+		black.alpha = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
